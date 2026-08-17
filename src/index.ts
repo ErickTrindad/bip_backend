2419af33cf0c0629c0a3dd44681f3eacbba30df9
Erick Trindade <erick.trindade2010@gmail.com>
2026-08-17 14:22:28 -0300

    feat: add allowedHeaders and methods to cors

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,8 @@ app.setSerializerCompiler(serializerCompiler);
 // CORS
 await app.register(cors, {
   origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ['Authorization', 'Content-Type', 'Accept']
 });
 
 // Swagger / OpenAPI Documentação
